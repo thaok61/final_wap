d64c08cecaa2a7334da333da3f2c32351314f64e
Vu Duc Thao <thao.vd1712@gmail.com>
2023-09-26 09:55:24 -0500

    update client port

--- final-project-client/app.js
+++ final-project-client/app.js
@@ -11,4 +11,4 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-module.exports = app;
+app.listen(8080, () => console.log('listening to 8080 ...'));
